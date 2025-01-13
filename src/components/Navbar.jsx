45f91ec1ae5227b108088cf8132648fc8ff542bb
Anto <mdnazirulmobinahamed@gmail.com>
2025-01-13 15:40:36 +0600

    more btn added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import user from '../assets/Nazirul Mobin.jpg'
 import { Button } from '@mui/material'
 import { Outlet, Link } from "react-router-dom";
 import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function Navbar() {
     const [activeComponent, setActiveComponent] = useState("home");
@@ -41,62 +42,67 @@ function Navbar() {
                         </div>
                     </div>
                     <div className='pt-2'>
-                        <button className="lg:hidden font-medium px-4 py-2 bg-slate-200 rounded-md" onClick={toggleDropdown}>
-                            {activeComponent.charAt(0).toUpperCase()+activeComponent.slice(1)}
-                        </button>
+                        <div className='flex justify-between'>
+                            <button className="lg:hidden font-medium px-4 py-2 bg-slate-200 rounded-md" onClick={toggleDropdown}>
+                                {activeComponent.charAt(0).toUpperCase() + activeComponent.slice(1)}
+                            </button>
+                            <button className="lg:hidden font-medium px-4 py-2 bg-slate-200 rounded-md" onClick={toggleDropdown}>
+                                <MoreHorizIcon fontSize='small' />
+                            </button>
+                        </div>
                         <ul className={`lg:flex flex-wrap font-medium ${dropdownOpen ? 'block' : 'hidden'} lg:block`}>
                             <Link to='/'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "home" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("home"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("home"); toggleDropdown(); }}
                                 >
                                     Home
                                 </li>
                             </Link>
                             <Link to='/about'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "about" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("about"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("about"); toggleDropdown(); }}
                                 >
                                     About
                                 </li>
                             </Link>
                             <Link to='/education'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "education" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("education"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("education"); toggleDropdown(); }}
                                 >
                                     Education
                                 </li>
                             </Link>
                             <Link to='/skills'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "skills" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("skills"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("skills"); toggleDropdown(); }}
                                 >
                                     Skills
                                 </li>
                             </Link>
                             <Link to='/projects'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "projects" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("projects"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("projects"); toggleDropdown(); }}
                                 >
                                     Projects
                                 </li>
                             </Link>
                             <Link to='/achivements'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "achivements" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("achivements"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("achivements"); toggleDropdown(); }}
                                 >
                                     Achivements
                                 </li>
                             </Link>
                             <Link to='/experiences'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "experiences" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("experiences"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("experiences"); toggleDropdown(); }}
                                 >
                                     Experiences
                                 </li>
                             </Link>
                             <Link to='/contacts'>
                                 <li className={`py-3 px-5 hover:bg-slate-100 rounded-md ${activeComponent === "contacts" ? "text-blue-600 border-b-4 border-b-blue-600 rounded-sm hover:bg-white" : "text-gray-500"}`}
-                                    onClick={() => {setActiveComponent("contacts"); toggleDropdown();}}
+                                    onClick={() => { setActiveComponent("contacts"); toggleDropdown(); }}
                                 >
                                     Contacts
                                 </li>
